abb64a780326b5506beebd1440c30ec4b70394bc
PetarMMihaylov <petar.mih97@gmail.com>
2025-11-02 12:22:11 +0200

    feat: claim submission functionality

--- src/main/java/app/claim/service/ClaimService.java
+++ src/main/java/app/claim/service/ClaimService.java
@@ -1,4 +1,10 @@
 package app.claim.service;
+import app.claim.model.Claim;
+import app.claim.model.ClaimStatus;
+import app.claim.repository.ClaimRepository;
+import app.user.model.User;
+import app.web.dto.ClaimSubmissionRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -6,4 +12,26 @@ import java.time.LocalDateTime;
 @Service
 public class ClaimService {
 
+    private final ClaimRepository claimRepository;
+
+    @Autowired
+    public ClaimService(ClaimRepository claimRepository) {
+        this.claimRepository = claimRepository;
+    }
+
+    public Claim createClaim(ClaimSubmissionRequest claimSubmissionRequest, User user) {
+
+        Claim claim = Claim.builder()
+                .claimType(claimSubmissionRequest.getClaimType())
+                .claimStatus(ClaimStatus.OPEN)
+                .requestedAmount(claimSubmissionRequest.getRequestedAmount())
+                .attachedDocument(claimSubmissionRequest.getAttachedDocument())
+                .description(claimSubmissionRequest.getDescription())
+                .createdOn(LocalDateTime.now())
+                .updatedOn(LocalDateTime.now())
+                .user(user)
+                .build();
+
+        return claimRepository.save(claim);
+    }
 }
--- src/main/java/app/web/ClaimController.java
+++ src/main/java/app/web/ClaimController.java
@@ -1,9 +1,60 @@
 package app.web;
+import app.claim.model.Claim;
+import app.claim.service.ClaimService;
+import app.security.AuthenticationMetadata;
+import app.user.model.User;
+import app.user.service.UserService;
+import app.web.dto.ClaimSubmissionRequest;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
 @RequestMapping("/claims")
 public class ClaimController {
+    private final ClaimService claimService;
+    private final UserService userService;
+
+    @Autowired
+    public ClaimController(ClaimService claimService, UserService userService) {
+        this.claimService = claimService;
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ModelAndView getNewClaimPage(@AuthenticationPrincipal AuthenticationMetadata authenticationMetadata) {
+
+        User user = userService.getById(authenticationMetadata.getUserId());
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("create-claim");
+        modelAndView.addObject("claimSubmissionRequest", new ClaimSubmissionRequest());
+        modelAndView.addObject("user", user);
+
+        return modelAndView;
+    }
+
+    @PostMapping
+    public ModelAndView submitClaim(@Valid ClaimSubmissionRequest claimSubmissionRequest, BindingResult bindingResult, @AuthenticationPrincipal AuthenticationMetadata authenticationMetadata) {
+
+        User user = userService.getById(authenticationMetadata.getUserId());
+
+        if (bindingResult.hasErrors()) {
+            ModelAndView modelAndView = new ModelAndView();
+            modelAndView.setViewName("create-claim");
+            modelAndView.addObject("user", user);
+            return modelAndView;
+        }
+
+        Claim claim = claimService.createClaim(claimSubmissionRequest, user);
+
+        return new ModelAndView("redirect:/claims");
+    }
 }
