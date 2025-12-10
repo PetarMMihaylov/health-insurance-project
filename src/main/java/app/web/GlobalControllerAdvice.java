79d7f018d5ef11fd39c79dd61a6fbe7fc3fc17a8
PetarMMihaylov <petar.mih97@gmail.com>
2025-12-10 21:41:29 +0200

    chore: minor changes

--- src/main/java/app/web/GlobalControllerAdvice.java
+++ src/main/java/app/web/GlobalControllerAdvice.java
@@ -2,13 +2,11 @@ package app.web;
 
 import app.exception.*;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.nio.file.AccessDeniedException;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
